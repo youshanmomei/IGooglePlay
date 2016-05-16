df1600e531db9052362eeabb51dbfb96b8fc25c9
qc <1142041743@qq.com>
2016-05-16 17:45:30 +0800

    1.BasicAdapter 实现

--- app/src/main/java/cn/qiuc/org/igoogleplay/ui/adapter/BasicAdapter.java
+++ app/src/main/java/cn/qiuc/org/igoogleplay/ui/adapter/BasicAdapter.java
@@ -1,21 +1,42 @@
 package cn.qiuc.org.igoogleplay.ui.adapter;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by admin on 2016/5/15.
  */
 public class BasicAdapter<T> extends BaseAdapter {
+
+    protected Context context;
+    protected ArrayList<T> list;
+
+    public BasicAdapter(Context context, ArrayList<T> list) {
+        this.context = context;
+        this.list = list;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return list.get(position);
     }
 
     @Override
@@ -26,5 +47,24 @@ public class BasicAdapter<T> extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
+    }
+
+    protected ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+
+    protected static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+
+        static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            if (loadedImage != null) {
+                ImageView imageView = (ImageView) view;
+                boolean fistDisplay = !displayedImages.contains(imageUri);
+                if (fistDisplay) {
+                    FadeInBitmapDisplayer.animate(imageView, 500);
+                    displayedImages.add(imageUri);
+                }
+            }
+        }
     }
 }
--- app/src/main/java/cn/qiuc/org/igoogleplay/ui/adapter/HomeAdapter.java
+++ app/src/main/java/cn/qiuc/org/igoogleplay/ui/adapter/HomeAdapter.java
@@ -1,10 +1,19 @@
 package cn.qiuc.org.igoogleplay.ui.adapter;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+
 import cn.qiuc.org.igoogleplay.bean.AppInfo;
 
 /**
  * Created by admin on 2016/5/15.
  */
 public class HomeAdapter extends BasicAdapter<AppInfo>{
-    //TODO
+
+    public HomeAdapter(Context context, ArrayList<AppInfo> list) {
+        super(context, list);
+    }
+
+    //TODO...
 }
